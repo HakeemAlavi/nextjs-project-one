3c042799c228d0411857714df350befae561e994
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-04-24 13:31:19 +0300

    feat: add Page to test todos DB table connection and functioning

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/utils/supabase/server'
 
 export default async function Page() {
   const supabase = createClient()
-  const { data: notes } = await supabase.from('notes').select()
+  const { data: todos } = await supabase.from('todos').select()
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  return <pre>{JSON.stringify(todos, null, 2)}</pre>
 }
