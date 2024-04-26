82f60ebf7f7877f3f0cf0a3fb7aec870d3300d4f
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-04-26 19:41:34 +0300

    feat: add Record entry logic

--- components/inputdemo.tsx
+++ components/inputdemo.tsx
@@ -1,11 +1,52 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+"use client"
+
+import React, { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { createClient } from "@/utils/supabase/client";
 
 export function InputDemo() {
+  const [taskInput, setTaskInput] = useState("");
+
+  // Function to handle form submission
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    // Check if taskInput is not empty
+    if (taskInput.trim() !== "") {
+      try {
+
+        const supabase = createClient();
+        // Insert the task into the 'todos' table
+        const { data, error } = await supabase
+          .from('todos')
+          .insert([{ task: taskInput }])
+          .select()
+
+        if (error) {
+          throw error;
+        }
+
+        // Log the result
+        console.log("Task created:", data[0].task);
+
+        // Clear the input field after submitting
+        setTaskInput("");
+      } catch (error) {
+        console.error("Error creating task");
+      }
+    }
+  };
+
   return (
-    <div className="flex w-full max-w-sm items-center space-x-2 m-4">
-      <Input type="text" placeholder="Do coding assignment" />
+    <form onSubmit={handleFormSubmit} className="flex w-full max-w-sm items-center space-x-2 m-4">
+      <Input 
+        type="text" 
+        placeholder="Do coding assignment" 
+        value={taskInput} // Set input value to the state
+        onChange={(e) => setTaskInput(e.target.value)} // Update the state when input changes
+      />
       <Button type="submit" className="bg-black text-white">Add Task</Button>
-    </div>
-  )
+    </form>
+  );
 }
